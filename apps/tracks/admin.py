2554d76fd1e2af64b5d1d3c216395d16a10400e1
Nathan Maniçoba <zaphiroxd5@gmail.com>
2026-05-12 00:25:12 -0300

    fix: adiciona rest_framework aos apps e registra models no admin

--- apps/tracks/admin.py
+++ apps/tracks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ChallengeSubmission, Module, Track, TrackChallenge, UserModuleProgress, UserTrack
+
+# Registrando os models de forma simples
+admin.site.register(Track)
+admin.site.register(Module)
+admin.site.register(TrackChallenge)
+admin.site.register(UserTrack)
+admin.site.register(UserModuleProgress)
+admin.site.register(ChallengeSubmission)
--- config/settings/base.py
+++ config/settings/base.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Local apps
+
+    "rest_framework",
+
     "apps.tracks",
 ]
 
