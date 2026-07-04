0d3d4fae05a5dcab4f6cf31ffd01000b2960336c
Nathan Maniçoba <zaphiroxd5@gmail.com>
2026-07-03 21:47:54 -0300

    fix: exceções nas viewsets

--- config/settings/base.py
+++ config/settings/base.py
@@ -108,7 +108,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ==============================================================================
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'EXCEPTION_HANDLER': 'apps.tracks.exceptions.custom_exception_handler',
 }
 
 SPECTACULAR_SETTINGS = {
